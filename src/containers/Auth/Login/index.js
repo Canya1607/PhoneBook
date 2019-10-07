4bbc5b1ebfad371486580d83d89066dd6457fc40
Canya1607 <frontend.dev6@inspiri.to>
2019-10-07 21:08:39 +0300

    Added inputs with image and navigate to SignUp

--- src/containers/Auth/Login/index.js
+++ src/containers/Auth/Login/index.js
@@ -1,13 +1,34 @@
+/* eslint-disable prettier/prettier */
 import React, {Component} from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import {SafeAreaView, View, Text, Image, TouchableOpacity} from 'react-native';
+import AuthInput from '../../../components/AuthInput';
+import AuthInputPass from '../../../components/AuthInputPass';
 import styles from './styles';
 
 class Login extends Component {
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <View>
-          <Text>Login</Text>
+      <SafeAreaView style={styles.screen}>
+        <View style={styles.container}>
+          <View style={styles.image}>
+            <Image
+              style={styles.phonebook}
+              source={require('../../../assets/images/phonebook.png')}
+            />
+          </View>
+          <AuthInput
+            text="Login"
+          />
+          <AuthInputPass
+            text="Password"
+          />
+        </View>
+        <View style={styles.bottom}>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate('SignUp')}>
+            <View>
+              <Text>Not Registered? Sign Up</Text>
+            </View>
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     );
--- src/containers/Auth/Login/styles.js
+++ src/containers/Auth/Login/styles.js
@@ -1,10 +1,25 @@
 import {StyleSheet} from 'react-native';
+import size from '../../../constants/size';
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#659DBD',
+  },
+  container: {
+    flex: 1,
+  },
+  image: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  phonebook: {
+    marginTop: size.Height * 0.2,
+    marginBottom: size.Height * 0.08,
+    width: size.Width * 0.25,
+    height: size.Width * 0.25,
   },
 });
 

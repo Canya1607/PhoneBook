92d0ceb13660e072bc4999a157f3e8b3190324dd
Canya1607 <frontend.dev6@inspiri.to>
2019-10-09 22:05:58 +0300

    Added simple usage of ImagePicker

--- src/containers/Main/Contacts/index.js
+++ src/containers/Main/Contacts/index.js
@@ -1,12 +1,46 @@
 import React, {Component} from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import {SafeAreaView, View, Text, Button} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 import styles from './styles';
 
 class Contacts extends Component {
+  myFucntion = () => {
+    const options = {
+      title: 'Select Avatar',
+      customButtons: [{name: 'fb', title: 'Choose Photo from Facebook'}],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+
+    ImagePicker.showImagePicker(options, response => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        const source = {uri: response.uri};
+
+        // You can also display the image using data:
+        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+        this.setState({
+          avatarSource: source,
+        });
+      }
+    });
+  };
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <View>
+          <Button title="Pick Image" onPress={() => this.myFucntion()} />
           <Text>Contacts</Text>
         </View>
       </SafeAreaView>

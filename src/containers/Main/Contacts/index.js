6f21e6d58a00275e2e0ea3b1f736e5718d8e27a1
Canya1607 <frontend.dev6@inspiri.to>
2019-10-10 20:50:26 +0300

    Added simple navigation to Details

--- src/containers/Main/Contacts/index.js
+++ src/containers/Main/Contacts/index.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
-import {SafeAreaView, View, Text, Button} from 'react-native';
+import {SafeAreaView, View, Text, Button, Image} from 'react-native';
 import ImagePicker from 'react-native-image-picker';
 import styles from './styles';
 
 class Contacts extends Component {
+  state = {
+    avatarSource: null,
+  };
+
   myFunction = () => {
     const options = {
       title: 'Select Avatar',
@@ -16,7 +20,6 @@ class Contacts extends Component {
 
     ImagePicker.showImagePicker(options, response => {
       console.log('Response = ', response);
-
       if (response.didCancel) {
         console.log('User cancelled image picker');
       } else if (response.error) {
@@ -24,11 +27,11 @@ class Contacts extends Component {
       } else if (response.customButton) {
         console.log('User tapped custom button: ', response.customButton);
       } else {
-        const source = {uri: response.uri};
+        // const source = {uri: response.uri};
 
         // You can also display the image using data:
-        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
-
+        const source = {uri: 'data:image/jpeg;base64,' + response.data};
+        console.log("URI" + response.uri);
         this.setState({
           avatarSource: source,
         });
@@ -37,11 +40,19 @@ class Contacts extends Component {
   };
 
   render() {
+    const {avatarSource} = this.state;
     return (
       <SafeAreaView style={styles.container}>
-        <View>
+        <View style={{flex: 1}}>
+          <Image style={{width: 300, height: 400}} source={avatarSource} />
           <Button title="Pick Image" onPress={() => this.myFunction()} />
           <Text>Contacts</Text>
+        </View>
+        <View>
+        <Button
+            title="Go to Details"
+            onPress={() => this.props.navigation.navigate('Details')}
+          />
         </View>
       </SafeAreaView>
     );

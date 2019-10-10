fdf5900aced75d4a2ab172243ec8c490b754a491
Canya1607 <frontend.dev6@inspiri.to>
2019-10-10 20:46:12 +0300

    Added <Divider> components with following types: Divider, FullDivider, HeightDivider, FlexDivider

---
+++ src/components/Divider/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View} from 'react-native';
+import styles from './styles';
+
+const Divider = () => {
+  return (
+    <View style={{alignItems: 'flex-end'}}>
+      <View style={styles.divider} />
+    </View>
+  );
+};
+
+const FullDivider = () => {
+  return <View style={styles.full_divider} />;
+};
+
+const HeightDivider = () => {
+  return <View style={styles.height_divider} />;
+};
+
+const FlexDivider = () => {
+  return <View style={styles.flex_divider} />;
+};
+
+export default {
+  Divider,
+  FullDivider,
+  HeightDivider,
+  FlexDivider,
+};

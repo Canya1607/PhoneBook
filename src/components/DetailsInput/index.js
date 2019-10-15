c881b43e1c47b2f8a6440ab21e0a20521a84448a
Canya1607 <hnenniy1610@gmail.com>
2019-10-15 18:21:54 +0300

    Added value propertyu

--- src/components/DetailsInput/index.js
+++ src/components/DetailsInput/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {TextInput} from 'react-native';
 import styles from './styles';
 
-const DetailsInput = ({placeholder}) => {
-  return <TextInput style={styles.input} placeholder={placeholder} />;
+const DetailsInput = ({placeholder, value}) => {
+  return (
+    <TextInput style={styles.input} placeholder={placeholder} value={value} />
+  );
 };
 
 export default DetailsInput;

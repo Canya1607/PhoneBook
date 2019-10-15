d2c1676570648e6781d7257b72fc2501804ed746
Canya1607 <hnenniy1610@gmail.com>
2019-10-15 18:25:14 +0300

    Added primitive usage of SectionList

--- src/containers/Main/Contacts/index.js
+++ src/containers/Main/Contacts/index.js
@@ -1,21 +1,54 @@
 import React, {Component} from 'react';
-import {SafeAreaView, View, Text, Button, Image} from 'react-native';
-import ImagePicker from 'react-native-image-picker';
+import {SafeAreaView, View, Button, Text, SectionList} from 'react-native';
+import Contact from '../../../components/Contact';
 import styles from './styles';
 
+const Data = [
+  {
+    title: 'A',
+    data: ['Anton', 'Alex'],
+  },
+  {
+    title: 'J',
+    data: ['Jack', 'Jamily', 'Jan'],
+  },
+  {
+    title: 'S',
+    data: ['Sasha', 'Slavik'],
+  },
+  {
+    title: 'V',
+    data: ['Vova', 'Vasya'],
+  },
+];
+
 class Contacts extends Component {
+  static navigationOptions = () => {
+    return {
+      title: 'Contacts',
+    };
+  };
+
   render() {
+    const {navigation} = this.props;
     return (
       <SafeAreaView style={styles.container}>
-        <View style={{flex: 1}}>
-          <Text>Contacts</Text>
-        </View>
-        <View>
+        <SectionList
+          sections={Data}
+          keyExtractor={(item, index) => item + index}
+          renderItem={({item}) => (
+            <Contact navigation={navigation} name={item} />
+          )}
+          renderSectionHeader={({section: {title}}) => (
+            <Text style={{fontSize: 32}}>{title}</Text>
+          )}
+        />
+        {/* <View>
           <Button
             title="Go to Details"
             onPress={() => this.props.navigation.navigate('Details')}
           />
-        </View>
+        </View> */}
       </SafeAreaView>
     );
   }

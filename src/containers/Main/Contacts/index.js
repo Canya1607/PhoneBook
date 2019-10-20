b35c5df680fa8db7166b69e12fe30e6df6735186
Canya1607 <hnenniy1610@gmail.com>
2019-10-20 19:12:39 +0300

    Changed to Flatlist

--- src/containers/Main/Contacts/index.js
+++ src/containers/Main/Contacts/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React, {Component} from 'react';
-import {SafeAreaView, Text, SectionList} from 'react-native';
+import {SafeAreaView, Text, SectionList, FlatList} from 'react-native';
 import Contact from '../../../components/Contact';
 import styles from './styles';
 
@@ -36,6 +36,19 @@ const Data = [
   },
 ];
 
+const data = [
+  {name: 'Anton', surname: 'Samkov', work: 'Designer', address: 'Zhovkivska 53', phone: '+380964821833'},
+  {name: 'Alex', surname: 'Babiy', work: 'IT STEP Student', address: 'Zhovkivska 54', phone: '+380947104745'},
+  {name: 'Jack', surname: 'Buyakov', work: 'Web Designer', address: 'Zhovkivska 55', phone: '+380964821833'},
+  {name: 'Jamily', surname: 'Samkov', work: 'Designer', address: 'Zhovkivska 56', phone: '+380964821833'},
+  {name: 'Jan', surname: 'Samkov', work: 'Designer', address: 'Zhovkivska 57', phone: '+380964821833'},
+  {name: 'Sasha', surname: 'Samkov', work: 'Designer', address: 'Zhovkivska 58', phone: '+380964821833'},
+  {name: 'Slavik', surname: 'Samkov', work: 'Designer', address: 'Zhovkivska 59', phone: '+380964821833'},
+  {name: 'Vova', surname: 'Samkov', work: 'Designer', address: 'Zhovkivska 67', phone: '+380964821833'},
+  {name: 'Vasya', surname: 'Samkov', work: 'Designer', address: 'Zhovkivska 72', phone: '+380964821833'},
+];
+
+
 class Contacts extends Component {
   static navigationOptions = () => {
     return {
@@ -47,7 +60,15 @@ class Contacts extends Component {
     const {navigation} = this.props;
     return (
       <SafeAreaView style={styles.container}>
-        <SectionList
+        <FlatList 
+          data={data}
+          initialNumToRender={9}
+          keyExtractor={(item, index) => item.name + index}
+          renderItem={({item}) => (
+            <Contact navigation={navigation} contact={item} />
+          )}
+        />
+        {/* <SectionList
           sections={Data}
           keyExtractor={(item, index) => item.name + index}
           renderItem={({item}) => (
@@ -56,7 +77,7 @@ class Contacts extends Component {
           renderSectionHeader={({section: {title}}) => (
             <Text style={{fontSize: 32}}>{title}</Text>
           )}
-        />
+        /> */}
         {/* <View>
           <Button
             title="Go to Details"

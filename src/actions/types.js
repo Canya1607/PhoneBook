8fdf2fb3ede51a5f20544febcdbc63b238b81cbd
Canya1607 <hnenniy1610@gmail.com>
2019-10-22 00:46:07 +0300

    Added all types for actions (Redux)

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,5 +1,14 @@
 // USERS
 
-
+export const ADD_USER = "ADD_USER_BY_OBJ";
+export const GET_USER = "GET_USER_BY_ID";
+export const GET_USERS = "GET_USERS";
+export const REMOVE_USER = "REMOVE_USER_BY_ID";
 
 // CONTACTS
+
+export const ADD_CONTACT = "ADD_CONTACT_BY_OBJ";
+export const GET_CONTACT = "GET_CONTACT_BY_ID";
+export const GET_CONTACTS = "GET_CONTACTS";
+export const REMOVE_CONTACT = "REMOVE_CONTACT_BY_OBJ";
+export const REMOVE_CONTACTS = "REMOVE_CONTACTS";

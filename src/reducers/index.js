f78788289ed6bccd936eca27a14276c1e1c5693f
Canya1607 <hnenniy1610@gmail.com>
2019-10-22 01:06:27 +0300

    Added skeleton for reducers and ready index.js

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import {combineReducers} from 'redux';
+import contacts from './contacts';
+import users from './users';
+
+const PhoneBook = combineReducers({
+    users,
+    contacts,
+});
+
+export default PhoneBook;
---
+++ src/reducers/users.js
@@ -0,0 +1,28 @@
+import {
+    ADD_USER,
+    GET_USER,
+    GET_USERS,
+    REMOVE_USER,
+} from '../actions/types';
+
+const defaultState = {
+    activeUser: null,
+    users: null, 
+};
+
+const users = (state = defaultState, action) => {
+    switch (action.type) {
+        case ADD_USER:
+            return
+        case GET_USER:
+            return
+        case GET_USERS:
+            return
+        case REMOVE_USER:
+            return
+        default:
+            return state;
+    }
+};
+
+export default users;
